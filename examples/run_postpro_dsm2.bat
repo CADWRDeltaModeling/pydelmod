d257d98b0a3037e19733d78e1ac74f2e225a4a4c
Brad Tom <Bradley.Tom@water.ca.gov>
2022-10-04 15:08:29 -0700

    updated to latest versions

--- examples/run_postpro_dsm2.bat
+++ examples/run_postpro_dsm2.bat
@@ -1,7 +1,15 @@
 setlocal
 call conda activate dev_pydelmod
-REM pydelmod exec-postpro-dsm2 observed postpro_cal_config.json --dask
+pydelmod exec-postpro-dsm2 observed postpro_cal_config.json --dask
 REM pydelmod exec-postpro-dsm2 model postpro_cal_config.json --dask
 REM pydelmod exec-postpro-dsm2 plots postpro_cal_config.json --dask
-pydelmod exec-postpro-dsm2 heatmaps postpro_cal_config.json --no-dask
+REM pydelmod exec-postpro-dsm2 heatmaps postpro_cal_config.json --no-dask
+
+REM colored maps for manning's/dispersion/length/all values
+REM If you specify a base file, the map will be colored by difference
+REM pydelmod map-channels-colored ..\..\..\postprocessing\grid_shapefile\dsm2_channels_straightlines_8_3.shp ..\output\hydro_echo_run_merged_ns28_c14_w52.inp --colored-by "MANNING" --base-file ..\..\hist19smcd\output\hydro_echo_hist_v82_19smcd.inp
+REM If you don't specify a base file, then the mannings value (not difference) is displayed
+REM pydelmod map-channels-colored ..\..\..\postprocessing\grid_shapefile\dsm2_channels_straightlines_8_3.shp ..\output\hydro_echo_run_merged_ns28_c14_w52.inp --colored-by "MANNING"
+REM 
+REM pydelmod map-channels-colored ..\..\..\postprocessing\grid_shapefile\dsm2_channels_straightlines_8_3.shp ..\output\hydro_echo_run_merged_ns28_c14_w52.inp --colored-by "DISPERSION"
 endlocal
