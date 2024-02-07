f66356530d164c494380d0f7571548424984694d
Nicky Sandhu <psandhu@water.ca.gov>
2024-02-06 16:24:53 -0800

    upgrading versioneer

--- pydelmod/__init__.py
+++ pydelmod/__init__.py
@@ -7,6 +7,5 @@ __email__ = 'knam@water.ca.gov'
 
 __all__ = ['nbplot', 'utilities']
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
